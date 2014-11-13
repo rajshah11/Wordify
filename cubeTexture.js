9d730501b0ccc51682860d08912b14cb559a9df6
rajshah11 <rajshah@iitgn.ac.in>
2014-11-14 03:20:20 +0530

    Now work from here

--- cubeTexture.js
+++ cubeTexture.js
@@ -21,6 +21,25 @@ function generateTexture(letter){
 	return texture;
 }
 
+function generateStartTexture(){
+	var bitmap = document.createElement('canvas');
+	var ctx = bitmap.getContext('2d');
+	bitmap.width = 200;
+	bitmap.height = 200;
+	ctx.font = '90px Calibri';
+	ctx.fillStyle = "#4ECDC4";
+	ctx.fillRect(0,0,200,200);
+
+	ctx.textAlign = "center";
+	ctx.fillStyle = "#FF6B6B";
+	ctx.fillText("start", 0.5*bitmap.width, 0.6*bitmap.width);
+	ctx.font = '40px Calibri';
+	
+	var texture = new THREE.Texture(bitmap) 
+	texture.needsUpdate = true;
+	return texture;
+}
+
 function generateplaneTexture(width,height){
 var bitmap = document.createElement('canvas');
 var ctx = bitmap.getContext('2d');
